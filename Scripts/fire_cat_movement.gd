a77c5c070ee20b7d554180297f9f9bfb390d1016
Finn <71734783+F1nn1125@users.noreply.github.com>
2025-08-11 11:55:32 +1200

    basic fire cat movement
    hopefully nothing's broken lol

---
+++ Scripts/fire_cat_movement.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@export var movement_speed: float
+var target_velocity := Vector2.ZERO
+
+func _physics_process(_delta):
+	var direction = Vector2.ZERO
+
+	if (Input.is_action_pressed("fire_cat_move_up")):
+		direction.y += -1
+		print("Fire cat move up")
+	if (Input.is_action_pressed("fire_cat_move_down")):
+		direction.y -= -1
+		print("Fire cat move down")
+	if (Input.is_action_pressed("fire_cat_move_left")):
+		direction.x -= 1
+		print("Fire cat move left")
+	if (Input.is_action_pressed("fire_cat_move_right")):
+		direction.x += 1
+		print("Fire cat move right")
+
+	target_velocity.x = direction.x * movement_speed
+	target_velocity.y = direction.y * movement_speed
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
