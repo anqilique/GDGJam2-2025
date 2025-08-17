bf367fc5eedd2eb41956abd7e4f45843e53cacb8
F1nn1125 <71734783+F1nn1125@users.noreply.github.com>
2025-08-17 12:03:07 +1200

    Made shadow scene + movement script
    put the players into a group called "Player" to find them ez on the
    shadow movement script

---
+++ Scripts/Shadow/ShadowMovement.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+@export var movement_speed: float
+var players
+var closest_player: Transform2D
+var time_since_closest_player_check_seconds: float
+
+func _ready():
+	players = get_tree().get_nodes_in_group("Player")
+	closest_player = find_closest_player()
+
+
+func _physics_process(_delta):
+	# Check for closest player every second, cuz this doesn't need to be every tick
+	if time_since_closest_player_check_seconds > 1.0:
+		closest_player = find_closest_player()
+		time_since_closest_player_check_seconds = 0
+	else:
+		time_since_closest_player_check_seconds += _delta
+
+	var angle = get_angle_to(closest_player.origin)
+
+	var direction: Vector2
+	direction.x = cos(angle)
+	direction.y = sin(angle)
+
+	var target_velocity: Vector2
+	target_velocity.x = direction.x * movement_speed
+	target_velocity.y = direction.y * movement_speed
+
+	velocity = target_velocity
+	move_and_slide()
+
+
+func find_closest_player():
+	var closest_player = players[0]
+	var closest_distance = transform.origin.distance_to(closest_player.transform.origin)
+
+	for player in players:
+		var distance = transform.origin.distance_to(player.transform.origin)
+		if distance < closest_distance:
+			closest_player = player
+			closest_distance = distance
+
+	return closest_player.transform
