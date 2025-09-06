908844377b86eec75216b088594d99e3008176e7
Finn <71734783+F1nn1125@users.noreply.github.com>
2025-09-06 15:43:14 +1200

    added timers where animations should be

--- Scripts/Shadow/States/ShadowAttack.gd
+++ Scripts/Shadow/States/ShadowAttack.gd
@@ -12,6 +12,7 @@ func enter():
 	yarn_cat_health.take_damage(damage)
 
 	# play anim here
+	await get_tree().create_timer(0.4).timeout
 
 	# when anim finished go back to moving:
 	shadow_state_machine.on_child_transition(shadow_state_machine.current_state, "ShadowMove")
--- Scripts/YarnCat/States/YCatHit.gd
+++ Scripts/YarnCat/States/YCatHit.gd
@@ -1,12 +1,17 @@
 extends State
 class_name YCatHit
 
+@export var yarn_cat_state_machine : StateMachine
 var cat
 
 func enter():
 	cat = get_tree().get_first_node_in_group("YarnCat")
 
+	await get_tree().create_timer(0.2).timeout
 	# play animation?
+
+	# when anim finished go back to idle:
+	yarn_cat_state_machine.on_child_transition(yarn_cat_state_machine.current_state, "YcatIdle")
 
 func exit():
 	pass
