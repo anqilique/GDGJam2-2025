b49b2b407f122349b20c892fcac384ae7238ab53
Finn <71734783+F1nn1125@users.noreply.github.com>
2025-09-13 11:04:16 +1200

    passing torch collisiong working

--- Scripts/YarnCat/Pass_Torch.gd
+++ Scripts/YarnCat/Pass_Torch.gd
@@ -2,7 +2,20 @@ extends Node
 
 @export var pass_area : Area2D
 
+var in_range : bool
+
 func _process(_delta):
-	if (Input.is_action_pressed("yarn_cat_pass_torch")):
-		print("workign")
+	if (Input.is_action_pressed("yarn_cat_pass_torch") and in_range):
+		print("passed torch!!!!!!!!!!!!!!!!!!!!!!!!!1")
 		pass
+
+
+func _on_area_entered(area2d: Area2D):
+	if area2d.get_parent().get_groups().has("FireCat"):
+		print("In range")
+		in_range = true
+
+
+func _on_area_exited(area2d: Area2D):
+	if area2d.get_parent().get_groups().has("FireCat"):
+		in_range = false
