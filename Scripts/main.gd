aadf16739e2850705ce3ad80d3e0944d0e2fbab1
anqilique <107088774+anqilique@users.noreply.github.com>
2025-09-13 14:00:58 +1200

    Camera Stuff

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,2 +1,20 @@
 extends Node2D
-# Test push comment.
+
+@export var shadow_scene: PackedScene
+
+
+func _process(delta: float) -> void:
+	if $ShadowSpawnTimer.is_stopped():
+		$ShadowSpawnTimer.start()
+
+
+func _on_shadow_spawn_timer_timeout() -> void:
+	var shadow = shadow_scene.instantiate()
+	
+	var shadow_spawn_location = $Camera2D/ShadowPath2D/ShadowSpawn
+	shadow_spawn_location.progress_ratio = randf()
+
+	shadow.position = shadow_spawn_location.position
+
+	add_child(shadow)
+	print("SPAWN")
