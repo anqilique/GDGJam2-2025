2323398e446ea2622ad75bb4e0dab44708d809a6
Finn <71734783+F1nn1125@users.noreply.github.com>
2025-09-13 14:16:54 +1200

    added camera scene and direction arrows in there

--- Scripts/CameraController.gd
+++ Scripts/CameraController.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
-@export var yarn_cat : Node2D
-@export var fire_cat : Node2D
+var yarn_cat : Node2D
+var fire_cat : Node2D
 
 const CAMERA_MOVE_SPEED := 0.03 # don't set higher than 1 lmao
+
+func _ready():
+	fire_cat = get_tree().get_first_node_in_group("FireCat")
+	yarn_cat = get_tree().get_first_node_in_group("YarnCat")
 
 func _process(_delta):
 	transform.origin = lerp(transform.origin, ((yarn_cat.transform.origin + fire_cat.transform.origin) / 2), CAMERA_MOVE_SPEED)
