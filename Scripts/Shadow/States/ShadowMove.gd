5bf704d588f251f442cc5f31d1ee571d65ff5b1e
Finn <71734783+F1nn1125@users.noreply.github.com>
2025-09-13 12:49:19 +1200

    shadow now only paths towards ycat but it gets scared as well

--- Scripts/Shadow/States/ShadowMove.gd
+++ Scripts/Shadow/States/ShadowMove.gd
@@ -5,12 +5,11 @@ class_name ShadowMove
 @export var shadow_node: Node2D
 @export var movement_speed: float
 
-var players
-var closest_player: Transform2D
+var players := []
+var yarn_cat : Node2D
 
 func enter():
-	players = (get_tree().get_nodes_in_group("YarnCat"))
-	closest_player = find_closest_player()
+	yarn_cat = get_tree().get_nodes_in_group("YarnCat")[0]
 
 
 func exit():
@@ -22,9 +21,7 @@ func update(_delta):
 
 
 func physics_update(_delta):
-	closest_player = find_closest_player()
-
-	var angle = shadow_character_body.get_angle_to(closest_player.origin)
+	var angle = shadow_character_body.get_angle_to(yarn_cat.transform.origin)
 
 	var max_range := 0.0
 	if Globals.torch_holder == "YarnCat":
@@ -38,21 +35,9 @@ func physics_update(_delta):
 	target_velocity.x = direction.x * movement_speed
 	target_velocity.y = direction.y * movement_speed
 
-	if shadow_node.transform.origin.distance_to(closest_player.origin) >= max_range:
+	if shadow_node.transform.origin.distance_to(yarn_cat.transform.origin) >= max_range:
 		shadow_character_body.velocity = target_velocity
 	else:
 		shadow_character_body.velocity = -target_velocity
 
 	shadow_character_body.move_and_slide()
-
-func find_closest_player():
-	var new_closest_player = players[0]
-	var closest_distance = shadow_character_body.transform.origin.distance_to(new_closest_player.transform.origin)
-
-	for player in players:
-		var distance = shadow_character_body.transform.origin.distance_to(player.transform.origin)
-		if distance < closest_distance:
-			new_closest_player = player
-			closest_distance = distance
-
-	return new_closest_player.transform
