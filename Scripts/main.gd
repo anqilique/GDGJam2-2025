430b84779ba07a3b7126eba5049187b081bab2d8
anqilique <107088774+anqilique@users.noreply.github.com>
2025-09-13 14:18:44 +1200

    Spawn Push

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
 @export var shadow_scene: PackedScene
+var fire_cat
+
+
+func _ready() -> void:
+	fire_cat = get_tree().get_first_node_in_group("FireCat")
 
 
 func _process(delta: float) -> void:
@@ -14,8 +19,10 @@ func _on_shadow_spawn_timer_timeout() -> void:
 	
 	var shadow_spawn_location = $Camera2D.global_position
 	
-	shadow_spawn_location.x += randf_range(-20, 20)
-	shadow_spawn_location.y += randf_range(-20, 20)
+	while abs(shadow_spawn_location.x - $Camera2D.global_position.x) <= 40:
+		shadow_spawn_location.x += randf_range(-100, 100)
+	while abs(shadow_spawn_location.y - $Camera2D.global_position.y) <= 40:
+		shadow_spawn_location.y += randf_range(-100, 100)
 
 	shadow.position = shadow_spawn_location
 
